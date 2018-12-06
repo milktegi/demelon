b8a89198e22c94819c23f5261dadf2c06b708a08
milktegi <milkteegi@gmail.com>
2018-12-06 16:57:14 +0900

    added login validation

---
+++ validation/login.js
@@ -0,0 +1,24 @@
+const Validator = require('validator');
+const isEmpty = require('./is-empty');
+
+module.exports = function validateLoginInput(data) {
+  let errors = {};
+
+  data.email = !isEmpty(data.email) ? data.email : '';
+  data.password = !isEmpty(data.password) ? data.password : '';
+
+  if (!Validator.isEmail(data.email)) {
+    errors.email = '유효하지 않은 이메일입니다';
+  }
+  if (Validator.isEmpty(data.email)) {
+    errors.email = '이메일을 바르게 입력해주세요';
+  }
+  if (Validator.isEmpty(data.password)) {
+    errors.password = '패스워드를 바르게 입력해주세요';
+  }
+
+  return {
+    errors,
+    isValid: isEmpty(errors)
+  };
+};
--- validation/register.js
+++ validation/register.js
@@ -4,9 +4,34 @@ const isEmpty = require('./is-empty');
 module.exports = function validateRegisterInput(data) {
   let errors = {};
 
+  data.name = !isEmpty(data.name) ? data.name : '';
+  data.email = !isEmpty(data.email) ? data.email : '';
+  data.password = !isEmpty(data.password) ? data.password : '';
+  data.password2 = !isEmpty(data.password2) ? data.password2 : '';
+
   if (!Validator.isLength(data.name, { min: 2, max: 30 })) {
     errors.name = '이름은 2글자 이상이어야 합니다';
   }
+
+  if (Validator.isEmpty(data.name)) {
+    errors.name = '이름을 바르게 입력해주세요';
+  }
+  if (Validator.isEmpty(data.email)) {
+    errors.email = '이메일을 바르게 입력해주세요';
+  }
+  if (Validator.isEmpty(data.password)) {
+    errors.password = '패스워드를 바르게 입력해주세요';
+  }
+  if (!Validator.isLength(data.password, { min: 6, max: 30 })) {
+    errors.password = '패스워드는 최소 6자리 이상입니다';
+  }
+   if (Validator.isEmpty(data.password2)) {
+    errors.password = '패스워드를 바르게 입력해주세요';
+  }
+  if (!Validator.equals(data.password, data.password2)) {
+    errors.password2 = '패스워드 불일치';
+  }
+
   return {
     errors,
     isValid: isEmpty(errors)
