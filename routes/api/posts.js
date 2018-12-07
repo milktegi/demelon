46fd4382436df9978d436938e26e46615f89e803
milktegi <milkteegi@gmail.com>
2018-12-07 20:09:36 +0900

    Added delete function

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -5,6 +5,7 @@ const passport = require('passport');
 
 /// moodel
 const Post = require('../../models/Post');
+const Profile = require('../../models/Profile');
 
 // validation
 const validatePostInput = require('../../validation/post');
@@ -45,7 +46,9 @@ router.get('/', (req, res) => {
   Post.find()
     .sort({ data: -1 })
     .then(posts => res.json(posts))
-    .catch(err => res.status(404).json({ nopostfound: '게시물이 존재하지 않습니다'}));
+    .catch(err =>
+      res.status(404).json({ nopostfound: '게시물이 존재하지 않습니다' })
+    );
 });
 
 //@route  Get api/posts/:id
@@ -55,7 +58,32 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   Post.findById(req.params.id)
     .then(post => res.json(post))
-    .catch(err => res.status(404).json({ nopostfound: '게시물이 존재하지 않습니다'}));
+    .catch(err =>
+      res.status(404).json({ nopostfound: '게시물이 존재하지 않습니다' })
+    );
+});
+
+//@route  Get api/delete
+//@desc   delete post
+//@access Private
+
+router.delete('/:id', passport.authenticate('jwt',{ session: false }), (req, res) => {
+  Profile.findOne({ user: req.user.id }).then(profile => {
+    Post.findById(req.params.id)
+      .then(post => {
+        // check for post owner
+        if (post.user.toString() !== req.user.id) {
+          return res
+            .status(401)
+            .json({ notauthorized: '자신의 게시글만 삭제할 수 있습니다' });
+        }
+        // delete
+        post.remove().then(() => res.json({ success: true }));
+      })
+      .catch(err =>
+        res.status(404).json({ postnofound: '해당 게시글을 찾을 수 없습니다.' })
+      );
+  });
 });
 
 module.exports = router;
