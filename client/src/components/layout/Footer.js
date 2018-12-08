8166fddcdd0ff86193668e1eea601970498c5090
milktegi <milkteegi@gmail.com>
2018-12-08 17:32:09 +0900

    created basic layout

---
+++ client/src/components/layout/Footer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+  render() {
+    return (
+      <div>
+        <footer class="bg-dark text-white mt-5 p-4 text-center">
+          Copyright &copy; 2018 Dev Connector
+        </footer>
+      </div>
+    );
+  }
+}
+
+export default Footer;
