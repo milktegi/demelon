994738a6e7e7f240f201d53664f68ffc444db238
milktegi <milkteegi@gmail.com>
2018-12-09 21:15:52 +0900

    Created profile comp with route & form field comps

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,7 @@ import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Dashboard from './components/dashboard/Dashboard';
+import CreateProfile from './components/create-profile/CreateProfile';
 import { setCurrentUser, logoutUser } from './actions/authActions';
 import { clearCurrentProfile } from './actions/profileActions';
 
@@ -44,6 +45,7 @@ class App extends Component {
               <Route exact path="/login" component={Login} />
               <Switch>
               <PrivateRoute exact path="/dashboard" component={Dashboard} />
+              <PrivateRoute exact path="/create-profile" component={CreateProfile} />
               </Switch>
             </div>
           </div>
