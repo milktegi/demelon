dbf0dbabd9839c0da5f59d0c84ed4689ec477b2c
milktegi <milkteegi@gmail.com>
2018-12-10 14:40:35 +0900

    displayed exp & edu dashboard + delete func

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -5,8 +5,11 @@ import { connect } from 'react-redux';
 import { getCurrentProfile, deleteAccount } from '../../actions/profileActions';
 import Spinner from '../common/Spinner';
 import ProfileActions from './ProfileActions';
+import Experience from './Experience';
+import Education from './Education';
 
 class Dashboard extends Component {
+
   componentDidMount = () => {
     this.props.getCurrentProfile();
   }
@@ -32,8 +35,10 @@ class Dashboard extends Component {
               <Link to={`/profile/${profile.handle}`}>{user.name}</Link>님 하염
             </p>
             <ProfileActions />
+            <Experience experience={profile.experience}/>
+            <Education education={profile.education}/>
             
-            <div style={{ marginBottom: '60ox'}}>
+            <div style={{ marginBottom: '60px'}}>
               <button onClick={this.onDeleteClick}
               className="btn btn-danger">
                 프로필 계정 삭제
--- client/src/components/edit-profile/EditProfile.js
+++ client/src/components/edit-profile/EditProfile.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
-import {withRouter} from 'react-router-dom';
+import {Link, withRouter} from 'react-router-dom';
 import PropTypes from 'prop-types';
 import TextFieldGroup from '../common/TextFieldGroup';
 import TextAreaFieldGroup from '../common/TextAreaFieldGroup';
@@ -31,7 +31,7 @@ class CreateProfile extends Component {
     };
   }
 
-  componentDidMount = () => {
+  componentDidMount() {
     this.props.getCurrentProfile();
   }
 
@@ -179,6 +179,9 @@ class CreateProfile extends Component {
         <div className='container'>
           <div className='row'>
             <div className='col-md-8 m-auto'>
+							<Link to="/dashboard" className="btn btn-light">
+                Go Back
+              </Link>
               <h1 className='display-4 text-center'>edit Your Profile</h1>
               <small className='d-block pb-3'>* = required fields</small>
               <form onSubmit={this.onSubmit}>
