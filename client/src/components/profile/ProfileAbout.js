800969f4f61bf9dd9580e04713be57ee3e5d5401
milktegi <milkteegi@gmail.com>
2018-12-11 00:40:17 +0900

    created Profile by handle & sub comps of it

---
+++ client/src/components/profile/ProfileAbout.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class ProfileAbout extends Component {
+	render() {
+		return (
+			<div>
+					<h2>todo : ProfileAbout </h2>
+			</div>
+		)
+	}
+}
+
+export default ProfileAbout;
